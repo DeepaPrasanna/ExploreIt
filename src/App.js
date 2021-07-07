6d91217cde66bc70fadd2f8dc0ef74815001bf4e
DeepaPrasanna <anneraj73@gmail.com>
2021-07-07 13:59:33 +0530

    project structure improved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import Unsplash from "../api/Unsplash";
-import Youtube from "../api/Youtube";
-import SearchBar from "./SearchBar";
-import ImageList from "./ImageList";
-import VideoDisplay from "./VideoDisplay";
-import ErrMsg from "./ErrMsg";
+import Unsplash from "./utils/api/Unsplash";
+import Youtube from "./utils/api/Youtube";
+import SearchBar from "./components/SearchBar";
+import ImageList from "./components/ImageList";
+import VideoDisplay from "./components/VideoDisplay";
+import ErrMsg from "./components/ErrMsg";
 
 const App = () => {
   const [images, setImages] = useState([]);
