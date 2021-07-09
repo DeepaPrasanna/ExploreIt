3620c94bf75a90ddc293ff30ffab6bf416a16808
DeepaPrasanna <anneraj73@gmail.com>
2021-07-09 22:00:38 +0530

    video container made responsive

--- src/components/VideoDisplay.js
+++ src/components/VideoDisplay.js
@@ -9,12 +9,13 @@ const VideoDisplay = ({ videos }) => {
     setSelectedVideo(video);
   };
   return (
-    <div className="ui grid">
+    <div className="ui stackable grid">
       <div className="row">
         <div className="eleven wide column">
           <VideoDetail video={selectedVideo ? selectedVideo : videos[0]} />
         </div>
-        <div className="five wide column">
+
+        <div className="five wide column ui stackable grid">
           <VideosList onVideoSelect={onVideoSelect} videos={videos} />
         </div>
       </div>
