a6aa4d5ce9f5e128674bc78c78b6d60573c40185
DeepaPrasanna <anneraj73@gmail.com>
2021-07-10 22:41:33 +0530

    masonary layout fixed

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,28 +1,8 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 
 const ImageCard = (props) => {
-  const [spans, setSpans] = useState(0);
-  const myRef = useRef(null);
-
-  useEffect(() => {
-    myRef.current.addEventListener("load", setSnaps);
-  }, []);
-
-  const setSnaps = () => {
-    const height = myRef.current.clientHeight;
-    const spans = Math.ceil(height / 20);
-    setSpans(spans);
-  };
-
   const { description, urls } = props.image;
 
-  return (
-    <img
-      style={{ gridRowEnd: `span ${spans}` }}
-      ref={myRef}
-      src={urls.regular}
-      alt={description}
-    />
-  );
+  return <img src={urls.regular} alt={description} />;
 };
 export default ImageCard;
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,27 @@
 import React from "react";
-import ImageCard from "./ImageCard";
+import Masonry from "react-masonry-css";
+
 import "../styles/ImageList.css";
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
   const images = props.images.map((image) => {
     return <ImageCard key={image.id} image={image} />;
   });
-  return <div className="image-list">{images}</div>;
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+  };
+  return (
+    <Masonry
+      breakpointCols={breakpointColumnsObj}
+      className="my-masonry-grid"
+      columnClassName="my-masonry-grid_column"
+    >
+      {images}
+    </Masonry>
+  );
 };
 
 export default ImageList;
